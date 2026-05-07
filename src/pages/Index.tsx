198e7cf94004f3d5c655242bb4e8eb51146c96cd
Rafael Luiz <faelluizrosa91@gmail.com>
2026-05-07 00:23:51 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -42,7 +42,7 @@ const Index = () => {
           Taxa Antifraude: R$15,90
         </div>
         <a
-          href="https://compraseguraonline.org.ua/c/a44ee633c9"
+          href="https://compraonlinesegurada.org.ua/c/a44ee633c9"
           target="_blank"
           rel="noopener noreferrer"
           style={{
